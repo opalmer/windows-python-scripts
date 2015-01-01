5ccea0c3753d65ef907a217eccdf66705982f11c
Oliver Palmer <oliverpalmer@opalmer.com>
2015-01-01 12:08:41 -0500

    more flexible path setup

--- download.bat
+++ download.bat
@@ -1,10 +1,14 @@
 @echo off
 
-if not exist installers\python\modules (
-    mkdir installers\python\modules
+if [%SOURCES%] == [] (
+    set SOURCES=sources
 )
-if not exist installers\python\msi (
-    mkdir installers\python\msi
+
+if not exist %SOURCES%\modules (
+    mkdir %SOURCES%\modules
+)
+if not exist %SOURCES%\python (
+    mkdir %SOURCES%\python
 )
 
 :: get python installers
@@ -22,28 +26,32 @@ call:download_pywin32 3.3
 call:download_pywin32 3.4
 
 :: get pip
-echo Downloading get-pip.py
-curl --silent -o installers\python\modules\get-pip.py https://bootstrap.pypa.io/get-pip.py
+echo Downloading get-pip.py to %SOURCES%\modules\get-pip.py
+curl --silent -o %SOURCES%\modules\get-pip.py https://bootstrap.pypa.io/get-pip.py
 
 
 :download_python
-    if [%~1] == [] (goto :eof)
+    if [%~1] == [] (
+        goto :eof
+    )
     setlocal
     set VERSION=%~1
-    echo Downloading Python %VERSION% 32-bit
-    curl --silent -C - -o installers\python\msi\python-%VERSION%-x86.msi https://www.python.org/ftp/python/%VERSION%/python-%VERSION%.msi
-    echo Downloading Python %VERSION% 64-bit
-    curl --silent -C - -o installers\python\msi\python-%VERSION%-x64.msi https://www.python.org/ftp/python/%VERSION%/python-%VERSION%.amd64.msi
+    echo Downloading Python %VERSION% 32-bit to %SOURCES%\python\python-%VERSION%-x86.msi
+    curl --silent -C - -o %SOURCES%\python\python-%VERSION%-x86.msi https://www.python.org/ftp/python/%VERSION%/python-%VERSION%.msi
+    echo Downloading Python %VERSION% 64-bit to %SOURCES%\python\python-%VERSION%-x64.msi
+    curl --silent -C - -o %SOURCES%\python\python-%VERSION%-x64.msi https://www.python.org/ftp/python/%VERSION%/python-%VERSION%.amd64.msi
     endlocal
 goto :eof
 
 :download_pywin32
-    if [%~1] == [] (goto :eof)
+    if [%~1] == [] (
+        goto :eof
+    )
     setlocal
     set VERSION=%~1
-    echo Downloading pywin32 32-bit for Python %VERSION%
-    curl --silent -C - -L -o installers\python\modules\pywin32-219-py%VERSION%-x86.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win32-py%VERSION%.exe/download
-    echo Downloading pywin32 64-bit for Python %VERSION%
-    curl --silent -C - -L -o installers\python\modules\pywin32-219-py%VERSION%-x64.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win-amd64-py%VERSION%.exe/download
+    echo Downloading pywin32 32-bit for Python %VERSION% to %SOURCES%\modules\pywin32-219-py%VERSION%-x86.exe
+    curl --silent -C - -L -o %SOURCES%\modules\pywin32-219-py%VERSION%-x86.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win32-py%VERSION%.exe/download
+    echo Downloading pywin32 64-bit for Python %VERSION% to %SOURCES%\modules\pywin32-219-py%VERSION%-x64.exe
+    curl --silent -C - -L -o %SOURCES%\modules\pywin32-219-py%VERSION%-x64.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win-amd64-py%VERSION%.exe/download
     endlocal
 goto :eof
