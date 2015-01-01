c5f828072ab409713f189a932a7b24d618585b1f
Oliver Palmer <oliverpalmer@opalmer.com>
2015-01-01 11:55:46 -0500

    use local variables

--- install_python.bat
+++ install_python.bat
@@ -27,21 +27,23 @@ curl --silent -o installers\python\modules\get-pip.py https://bootstrap.pypa.io/
 
 
 :download_python
-    if not "%~1" == "" (
-        set PYTHON_VERSION=%~1
-        echo Downloading Python %PYTHON_VERSION% 32-bit
-        curl --silent -C - -o installers\python\msi\python-%PYTHON_VERSION%-x86.msi https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%.msi
-        echo Downloading Python %PYTHON_VERSION% 64-bit
-        curl --silent -C - -o installers\python\msi\python-%PYTHON_VERSION%-x64.msi https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%.amd64.msi
-    )
+    if [%~1] == [] (goto :eof)
+    setlocal
+    set VERSION=%~1
+    echo Downloading Python %VERSION% 32-bit
+    curl --silent -C - -o installers\python\msi\python-%VERSION%-x86.msi https://www.python.org/ftp/python/%VERSION%/python-%VERSION%.msi
+    echo Downloading Python %VERSION% 64-bit
+    curl --silent -C - -o installers\python\msi\python-%VERSION%-x64.msi https://www.python.org/ftp/python/%VERSION%/python-%VERSION%.amd64.msi
+    endlocal
 goto :eof
 
 :download_pywin32
-    if not "%~1" == "" (
-        set PYWIN_VERSION=%~1
-        echo Downloading pywin32 32-bit for Python %PYWIN_VERSION%
-        curl --silent -C - -L -o installers\python\modules\pywin32-219-py%PYWIN_VERSION%-x86.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win32-py%PYWIN_VERSION%.exe/download
-        echo Downloading pywin32 64-bit for Python %PYWIN_VERSION%
-        curl --silent -C - -L -o installers\python\modules\pywin32-219-py%PYWIN_VERSION%-x64.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win-amd64-py%PYWIN_VERSION%.exe/download
-    )
+    if [%~1] == [] (goto :eof)
+    setlocal
+    set VERSION=%~1
+    echo Downloading pywin32 32-bit for Python %VERSION%
+    curl --silent -C - -L -o installers\python\modules\pywin32-219-py%VERSION%-x86.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win32-py%VERSION%.exe/download
+    echo Downloading pywin32 64-bit for Python %VERSION%
+    curl --silent -C - -L -o installers\python\modules\pywin32-219-py%VERSION%-x64.exe http://sourceforge.net/projects/pywin32/files/pywin32/Build%%20219/pywin32-219.win-amd64-py%VERSION%.exe/download
+    endlocal
 goto :eof
