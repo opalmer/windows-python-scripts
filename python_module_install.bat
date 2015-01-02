49410976515f635e9924e7661267dd2f08bbc6f7
Oliver Palmer <oliverpalmer@opalmer.com>
2015-01-02 00:09:18 -0500

    adding pywin32_install target

--- python_module_install.bat
+++ python_module_install.bat
@@ -23,8 +23,9 @@ for /D %%d in (%PREFIX%\*) do (
 :: install additional python package(s)
 for /D %%d in (%PREFIX%\*) do (
     call:pip_install "%%d" virtualenv
+    call:pip_install "%%d" setuptools
+    call:pip_install "%%d" wheel
 )
-
 
 :get_pip
     if [%~1] == [] (goto :eof)
@@ -57,3 +58,30 @@ goto :eof
     endlocal
 goto :eof
 
+
+:pywin32_install
+    if [%~1] == [] (goto :eof)
+    if [%~2] == [] (goto :eof)
+    if [%~3] == [] (goto :eof)
+    setlocal
+    set VERSION=%~1
+    set SHORT_VERSION=%~2
+    set ARCH=%~3
+    set PYTHON=%PREFIX%\%VERSION%-%ARCH%\python.exe
+    set PYWIN32_INSTALLER=%SOURCES%\modules\pywin32-219-py%SHORT_VERSION%-%ARCH%.exe
+    set OUTPUT_DIR=%SOURCES%\modules\pywin32-219-py%SHORT_VERSION%-%ARCH%
+    if not exist %PYTHON% (
+        echo ERROR: %PYTHON% does not exist
+        goto :eof
+    )
+    if not exist %PYWIN32_INSTALLER% (
+        echo ERROR: %PYWIN32_INSTALLER% does not exist
+        goto :eof
+    )
+    if exist %OUTPUT_DIR% (
+        rd /s /q %OUTPUT_DIR%
+    )
+    echo Installing %PYWIN32_INSTALLER%
+    %PYWIN32_INSTALLER%
+    endlocal
+goto :eof
